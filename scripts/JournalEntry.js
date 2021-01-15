2441a94e04f37494ee5708608e6f1977d1c16efa
dtracydepp <tracydepp82@gmail.com>
2021-01-15 16:02:01 -0600

    added header for journal entries

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -6,7 +6,7 @@
 export const JournalEntryComponent = (entry) => {
     return `
         <section id="entry--${entry.id}" class="journalEntry">
-            Display the entry's full text, and the date
+           Display the entry's full text, and the date
             it was entered here.
         </section>
     `
